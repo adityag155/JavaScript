ab05b54818014278afc3322eaf7aed276b33e0ce
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-04-30 12:44:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+//Operator of JS
+
+//Arithmetic Operators:
+
+/*
+let a=5;
+let b=2;
+
+console.log("a=",a,"& b=",b);
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+
+console.log(a**b);          //5^2=25
+*/
+
+
+//Unary Operator: [a++,++a,a--,--a]
+/*
+let a=5;
+let b=2;
+console.log(a++);
+console.log(a);
+
+console.log(++a);
+console.log(a);
+
+console.log(a--);
+console.log(a);
+
+console.log(--a);
+console.log(a);
+*/
+
+//Assignment Operator:[=,+=,*=,%=,**=]
+/*
+let a=5;
+let b=2;
+
+a += 4;
+console.log(a);                 //a=a+4=9
+
+a-=4
+console.log(a);
+*/
+
+//Comparison Operator:[==,!=,===,!==,>,>=,<,<=]
+/*
+let a=5;
+let b=2;
+
+console.log(a==b);                  //f
+console.log(a!=b);                  //t
+console.log(a===b);                 //f
+console.log(a!==b);                 //t
+*/
+
+//Logical Operators:[&&,||,!]
+/*
+let a=5;
+let b=2;
+
+let cond1=a>b;
+let cond2=a===5;
+console.log(cond1 && cond2);            //t
+
+let cond1=a>b;
+let cond2=b>a;
+console.log(cond1 || cond2);
+
+console.log(!(a>b));                //f
+console.log(!(a<b));                //t
+*/
+
+//Conditional Statements:[if,if-else,]
+/*
+let age=23;
+if(age>18)
+{
+    console.log("You can vote");
+}
+if(age<18){
+    console.log("Your are under 18");
+}
+*/
